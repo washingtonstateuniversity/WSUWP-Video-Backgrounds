be8f6335ba5d43e5dfcc6a223ee69c489c116dbf
jeremyfelt <jeremy.felt@gmail.com>
2014-07-21 14:57:32 -0700

    Add initial plugin file

---
+++ wsu-video-backgrounds.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: WSUWP Video Backgrounds
+Version: 0.0.1
+Plugin URI: http://web.wsu.edu
+Description: A WordPress plugin to display HTML5 video backgrounds.
+Author: washingtonstateuniversity, jeremyfelt
+Author URI: http://web.wsu.edu
+*/
+
+class WSU_Video_Background {
+	/**
+	 * Setup plugin hooks.
+	 */
+	public function __construct() {
+
+	}
+}
+new WSU_Video_Background();
