8978e0fd553001775c80a55d3ada8f88386bcdac
jeremyfelt <jeremy.felt@gmail.com>
2014-07-21 15:10:47 -0700

    Start initial handling of the video background shortcode

--- wsu-video-backgrounds.php
+++ wsu-video-backgrounds.php
@@ -13,7 +13,44 @@ class WSU_Video_Background {
 	 * Setup plugin hooks.
 	 */
 	public function __construct() {
+		add_shortcode( 'wsu_video_background', array( $this, 'video_background' ) );
+	}
 
+	/**
+	 * Display a video background container.
+	 *
+	 * @param array  $atts    List of attributes to use in the shortcode.
+	 * @param string $content Content provided between shortcode tags.
+	 *
+	 * @return string
+	 */
+	public function video_background( $atts, $content = null ) {
+		$defaults = array(
+			'id'          => 'wsu-video-background',
+			'mp4'         => '',
+			'ogv'         => '',
+			'webm'        => '',
+			'poster'      => '',
+			'autoplay'    => true,
+			'loop'        => true,
+			'scale'       => true,
+			'zIndex'      => 0,
+			'opacity'     => 1,
+			'script_only' => false,
+		);
+		$atts = shortcode_atts( $defaults, $atts );
+
+		ob_start();
+
+		// By default, create the container for the video background.
+		if ( false === $atts['script_only'] ) {
+			?><div id="<?php echo esc_attr( $atts['id'] ); ?>"><?php if ( null !== $content ) { echo apply_filters( 'the_content', $content ); } ?></div><?php
+		}
+
+		$content = ob_get_contents();
+		ob_end_clean();
+
+		return $content;
 	}
 }
 new WSU_Video_Background();
